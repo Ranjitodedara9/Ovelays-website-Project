6066786ed7b0133971b2ae0aa6d4464f1894527c
Dharmikodii <[ajitodedara68@gmail.com]>
2024-08-31 15:02:10 +0530

    filter section update

--- overlays-web/src/Pages/ShopAll.jsx
+++ overlays-web/src/Pages/ShopAll.jsx
@@ -8,7 +8,9 @@ import { IoIosArrowDown } from "react-icons/io";
 const ShopAll = () => {
   const [allProducts, setAllProd] = useState([]);
   const [filterprod, setfilterprod] = useState([]);
+  const [showPriceBox, setshowPriceBox] = useState(false);
   const [paging, setpagig] = useState(1);
+  const [ProdType, setProdType] = useState([]);
 
   const lastelem = paging * 6;
   const firstelem = lastelem - 6;
@@ -30,6 +32,16 @@ const ShopAll = () => {
   const pagig = Math.ceil(filterprod.length / 6);
   const pagigArr = new Array(pagig).fill(pagig);
 
+  const showpriceBox = () => {
+    setshowPriceBox(!showPriceBox);
+  };
+  const ShowProdType = () => {
+    const GetType = allProducts.map((val) => val.prodinfo);
+
+    const dup = [...new Set(GetType)];
+    setProdType(dup);
+  };
+
   return (
     <>
       <div>
@@ -40,20 +52,73 @@ const ShopAll = () => {
         </div>
         <div className="flex flex-col justify-center  items-start lep:grid lep:grid-cols-[auto_1fr]">
           <div className="lep:sticky lep:top-[28vh]   w-[25vw] h-screen hidden lep:block">
-            <div className="w-full flex flex-col    h-full">
+            <div className="w-full flex flex-col    h-full ">
               <div className="ms-3 mt-2 ">
                 <h1 className=" font-normal text-2xl capitalize ">Filters</h1>
                 <hr className="w-[90%] my-2" />
-                <div className="me-4 mt-3">
-                  <span className="flex justify-between text-xl items-center font-semibold">
-                    price{" "}
+                <div className="me-4 mt-3 p-1 ">
+                  <span
+                    onClick={showpriceBox}
+                    className="flex justify-between text-lg  items-center font-bold">
+                    Price{" "}
+                    <i className="">
+                      <IoIosArrowDown />
+                    </i>
+                  </span>
+                  {showPriceBox && (
+                    <div className="flex justify-center p-2 flex-col">
+                      <input
+                        type="range"
+                        min={999}
+                        max={5000}
+                        value={999}
+                      />
+
+                      <div className="flex justify-center items-center gap-2  mt-2">
+                        <div className="flex justify-center items-center gap-1 border-1 h-8">
+                          <LiaRupeeSignSolid />
+                          <input
+                            type="text"
+                            className="w-[50px] "
+                          />
+                        </div>
+                        to
+                        <div className="h-8 flex border-1 justify-center items-center gap-1">
+                          <LiaRupeeSignSolid />
+                          <input
+                            type="text"
+                            className="w-[50px] "
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+                <hr className="w-[90%] my-2" />
+                <div className="me-4 mt-3 p-1">
+                  <span
+                    className="flex justify-between text-lg items-center font-bold"
+                    onClick={ShowProdType}>
+                    Product Type{" "}
+                    <i className="">
+                      <IoIosArrowDown />
+                    </i>
+                  </span>
+                  <div className="flex flex-col mt-2">
+                    {ProdType.map((Type) => (
+                      <span>{Type}</span>
+                    ))}
+                  </div>
+                </div>
+                <hr className="w-[90%] my-2" />
+                <div className="me-4 mt-3 p-1">
+                  <span className="flex justify-between text-lg items-center font-bold">
+                    Sizes{" "}
                     <i className="">
                       <IoIosArrowDown />
                     </i>
                   </span>
                 </div>
-
-                <hr className="w-[90%] my-2 mt-3" />
               </div>
             </div>
           </div>
